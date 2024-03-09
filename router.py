7ccbc37d9389fc20f45482f6d4fe880aba46cd9b
routpalm <nanthon7@uoregon.edu>
2024-03-09 12:47:12 -0800

    route propagation added

--- router.py
+++ router.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import json
-from simple_blockchain import Blockchain, Block
+import time
 
 
 class EBGPRouter:
@@ -11,23 +11,33 @@ class EBGPRouter:
         self.port = port
         self.neighbors = {}  # neighbor IP is key and AS number is value
         self.routes = []  # list of routes advertised by this router
+        self.routing_table = {}  # routing table mapping prefixes to paths
 
-    def add_neighbor(self, neighbor_ip, neighbor_as):
-        self.neighbors[neighbor_ip] = neighbor_as
+    def add_neighbor(self, neighbor_ip, neighbor_as, neighbor_port):
+        self.neighbors[neighbor_ip] = (neighbor_as, neighbor_port)
 
     def advertise_route(self, route):
         self.routes.append(route)
+        # update routing table with this new route, assuming self path is the best
+        self.routing_table[route] = [self.as_number]
         for neighbor in self.neighbors:
-            self.send_route(route, neighbor)
+            self.send_route(route, neighbor, self.ip)
 
-    def send_route(self, route, neighbor_ip):
+    def send_route(self, route, neighbor_ip, source_ip):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                s.connect((neighbor_ip, self.neighbors[neighbor_ip]))  # use the neighbor's port
-                route_info = json.dumps({"route": route, "as_number": self.as_number})
+                neighbor_as, neighbor_port = self.neighbors[neighbor_ip]
+                s.connect((neighbor_ip, neighbor_port))  # use the neighbor's port
+                route_info = json.dumps({"route": route, "as_number": self.as_number, "source_ip": source_ip})
                 s.sendall(route_info.encode('utf-8'))
+                print(f"as {self.as_number} sending route to {neighbor_ip}")
             except ConnectionRefusedError:
-                print(f"connection to {neighbor_ip} refused")
+                print(f"Connection to {neighbor_ip} refused")
+
+    def routing_decision(self, route_info, route):
+        # update routing table if new or shorter path (w/o blockchain validation)
+        if route not in self.routing_table or len(self.routing_table[route]) > len(route_info['path']) + 1:
+            self.routing_table[route] = [self.as_number] + route_info.get('path', [])
 
     def handle_client(self, connection, address):
         try:
@@ -35,11 +45,22 @@ class EBGPRouter:
                 received_data = connection.recv(1024).decode('utf-8')
                 if received_data:
                     route_info = json.loads(received_data)
-                    print(f"received route {route_info['route']} from AS {route_info['as_number']}")
+                    route = route_info['route']
+                    as_number = route_info['as_number']
+                    source_ip = route_info.get('source_ip', address[0])
+                    print(f"AS {self.as_number} received route {route} from AS {as_number}")
 
+                    # would be overwritten in securebgp router
+                    self.routing_decision(route_info, route)
+
+                    # forward to all neighbors except the source
+                    for neighbor_ip in self.neighbors:
+                        neighbor_as, neighbor_port = self.neighbors[neighbor_ip]
+                        if neighbor_as != as_number:  # check to avoid sending back to the source
+                            self.send_route(route, neighbor_ip,self.ip)
 
         except Exception as e:
-            print(f"an error occurred in handle_client thread: {e}")
+            print(f"An error occurred in handle_client thread: {e}")
 
     def listen_for_routes(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -51,8 +72,39 @@ class EBGPRouter:
                     client_thread = threading.Thread(target=self.handle_client, args=(conn, addr))
                     client_thread.start()
             except Exception as e:
-                print(f"error occurred in listen_for_routes: {e}")
+                print(f"Error occurred in listen_for_routes: {e}")
 
     def start(self):
         listener_thread = threading.Thread(target=self.listen_for_routes)
         listener_thread.start()
+
+"""
+    TEST METHODS
+"""
+def setup_routers():
+    router1 = EBGPRouter('127.0.0.1', 65001, 5001)
+    router2 = EBGPRouter('127.0.0.1', 65002, 5002)
+    router3 = EBGPRouter('127.0.0.1', 65003, 5003)
+
+    router1.add_neighbor('127.0.0.1', 65002, 5002)  # r1 -> r2
+    router2.add_neighbor('127.0.0.1', 65001, 5001)  # r2 -> r1
+    router2.add_neighbor('127.0.0.1', 65003, 5003)  # r2 -> r3
+    router3.add_neighbor('127.0.0.1', 65002, 5002)  # r3 -> r2
+
+    return router1, router2, router3
+def start_routers(routers):
+    for router in routers:
+        router.start()
+def verify_routes(routers, advertised_route):
+    time.sleep(2)
+    for router in routers:
+        if advertised_route in router.routing_table:
+            print(f"Router AS{router.as_number} knows about route {advertised_route}")
+        else:
+            print(f"Router AS{router.as_number} does NOT know about route {advertised_route}")
+
+# test
+router1, router2, router3 = setup_routers()
+start_routers([router1, router2, router3])
+router1.advertise_route("192.168.1.0/24")
+verify_routes([router1, router2, router3], "192.168.1.0/24")
